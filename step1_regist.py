ec4570a78c3c6df693bca5a47aaf3ca774e66f51
Gerardo Gonzalez <jpgrgmgs@gmail.com>
2016-10-02 01:35:12 -0700

    added comments

--- step1_regist.py
+++ step1_regist.py
@@ -1,4 +1,9 @@
+# Step 1: Registration
+
 import requests
 
+# Fill body of http request
 body = {'token': '299d3c92524b9177499249cc21e6da28', 'github': 'https://github.com/gmgonzal/code2040'}
+
+# POST request
 response = requests.post("http://challenge.code2040.org/api/register", data=body)
